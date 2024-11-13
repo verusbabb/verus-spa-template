e416f06a14653ae4b6d91a8260f59d770d5b976d
verusbabb <steve.babb@outlook.com>
2024-11-13 07:02:40 -0600

    initial scaffolding

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Material from "@primevue/themes/material";
+
+const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Material,
+  },
+});
+
+app.mount("#app");
